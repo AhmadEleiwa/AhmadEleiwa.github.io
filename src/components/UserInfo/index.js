77145e72c785fd57af2ec282a18c0bc930338145
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-06 23:46:06 +0300

    Create UserInof componetn section

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,20 @@
+import Typography from "../Typography"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapPin, faHandsClapping } from '@fortawesome/free-solid-svg-icons'
+
+import './style.css'
+
+const UserInfo = props => {
+    return <div className="user-info">
+        <div>
+            <Typography type='title'>Ahmad Ilawa <FontAwesomeIcon icon={faHandsClapping} /></Typography>
+            <Typography type='subtitle'>Full-stack Developer</Typography>
+            <Typography type='p'>A passionate full-stack developer from palestine  <FontAwesomeIcon icon={faMapPin} /></Typography>
+
+        </div>
+        <img src='assets/profile-image.jpg' alt='ahmad eleiwa' />
+    </div>
+}
+
+export default UserInfo
