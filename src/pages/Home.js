5c729306ee8c1cce89c62d476f195ff91c5b8af7
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-06 23:15:41 +0300

    Add About section into home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
+
 import Header from "../components/Header"
 
-const Home = props =>{
-    return <>
-    <Header />
-    </>
+import Aboutme from "../components/Aboutme"
+import Container from "../components/Container"
+
+const Home = props => {
+    return <div>
+        <Header />
+        <Container>
+            <Aboutme />
+        </Container>
+
+    </div>
 }
 
 export default Home
