422fda14f2e7bf2e63154e2762b385fcb0ebaa50
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-06 19:49:18 +0300

    Add header Logo

---
+++ src/components/Header/Logo/index.js
@@ -0,0 +1,9 @@
+const Logo = props => {
+    return <div class="menu-div">
+        <h1> <img src="https://img.icons8.com/windows/48/efefef/dev.png" alt="dev logo" /> AhmadIlawa </h1>
+        <img class="menu-icon" onclick="menuIconHandler()"
+            src="https://img.icons8.com/ios-glyphs/30/efefef/menu-rounded.png" alt="menu-icon" />
+    </div>
+}
+
+export default Logo
