bc24dd1390643de8676656170165b62b0f633766
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-06 23:49:17 +0300

    Add user info section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,13 @@ import Header from "../components/Header"
 
 import Aboutme from "../components/Aboutme"
 import Container from "../components/Container"
+import UserInfo from "../components/UserInfo"
 
 const Home = props => {
     return <div>
         <Header />
         <Container>
+            <UserInfo />
             <Aboutme />
         </Container>
 
