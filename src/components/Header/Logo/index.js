6e5cbf01b94543d82d547a1dbddd44df5ce5a94c
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-06 20:46:01 +0300

    Change the img logo to fontawesome icon

--- src/components/Header/Logo/index.js
+++ src/components/Header/Logo/index.js
@@ -1,8 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+
 const Logo = props => {
-    return <div class="menu-div">
-        <h1> <img src="https://img.icons8.com/windows/48/efefef/dev.png" alt="dev logo" /> AhmadIlawa </h1>
-        <img class="menu-icon" onclick="menuIconHandler()"
-            src="https://img.icons8.com/ios-glyphs/30/efefef/menu-rounded.png" alt="menu-icon" />
+    return <div style={{ display: 'flex', alignItems: 'center', color: 'white', gap: '0.2em' }}>
+        <FontAwesomeIcon icon={faCode} fontSize={'1.2em'} />
+        <h1 style={{fontSize:'1.2em'}}>  AhmadIlawa </h1>
     </div>
 }
 
