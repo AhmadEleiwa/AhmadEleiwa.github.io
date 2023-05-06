13cb4b2db1f7cff582b8f23c00fdc1409a7053ca
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-06 19:46:43 +0300

    Add Header Component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import Logo from "./Logo"
+import NavLinks from "./NavLinks"
+
+import './style.css'
+const Header = props => {
+    return <header>
+        <Logo />
+        <NavLinks />
+    </header>
+}
+
+
+export default Header
