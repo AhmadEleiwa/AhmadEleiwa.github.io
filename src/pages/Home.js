8939fd9fa26642ccb879148da625655511585616
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-07 14:03:37 +0300

    Fix conflict

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,27 @@
-const Home = props =>{
-    return <>
-    </>
+
+import Header from "../components/Header"
+
+import Aboutme from "../components/Aboutme"
+import Container from "../components/Container"
+import UserInfo from "../components/UserInfo"
+import Skills from "../components/Skills"
+import Projects from "../components/Projects"
+import ContactForm from "../components/ContactForm"
+import Footer from "../components/Footer"
+
+const Home = props => {
+    return <div>
+        <Header />
+        <Container>
+            <UserInfo />
+            <Aboutme />
+            <Skills />
+            <Projects />
+            <ContactForm />
+        </Container>
+        <Footer />
+
+    </div>
 }
 
 export default Home
