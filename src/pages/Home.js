e2563baf2e30e81c7619db286cc46323b1f0b3da
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-07 12:43:55 +0300

    projects section added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Aboutme from "../components/Aboutme"
 import Container from "../components/Container"
 import UserInfo from "../components/UserInfo"
 import Skills from "../components/Skills"
+import Projects from "../components/Projects"
 
 const Home = props => {
     return <div>
@@ -13,6 +14,7 @@ const Home = props => {
             <UserInfo />
             <Aboutme />
             <Skills />
+            <Projects />
         </Container>
 
     </div>
